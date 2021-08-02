e9797fc67dde6181ce35a04d8807d950eb7c8240
srajpal <srajpal@3clogic.com>
2021-08-02 19:59:18 +0530

    commit

---
+++ my-app/src/components/Users/UserItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const UserItem = ({ user: { avatar_url, login, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        alt='img'
+        src={avatar_url}
+        className='round-img'
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <Link className='btn btn-dark btn-sm my-1' to={`user/${login}`}>
+          More
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default UserItem;
---
+++ my-app/src/components/layout/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+const Navbar = (props) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={props.icon} /> {props.title}
+      </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
+Navbar.defaultProps = {
+  title: "Static title",
+};
+
+export default Navbar;
